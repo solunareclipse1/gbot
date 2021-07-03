f285f968720e0e735f537763b18fab4018a7ec10
solunareclipse1 <26941050+solunareclipse1@users.noreply.github.com>
2021-07-02 19:21:50 -0500

    mute functionality mostly complete, doesnt check for permissions as of now

--- cogs/debug.py
+++ cogs/debug.py
@@ -4,19 +4,17 @@ from common import config, log
 from discord.ext import commands, tasks
 
 ## General utility commands
-class utilsCog(commands.Cog):
+class debugCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     ## Test latency
     @commands.command()
-    @commands.is_owner()
     async def ping(self, ctx):
         await ctx.send(f'Ping! Took ~{round (self.bot.latency * 1000)}ms')
 
     ## Retreive user information
     @commands.command()
-    @commands.is_owner()
     async def info(self, ctx):
         await ctx.send(f'{self.bot.user}')
 
@@ -28,4 +26,4 @@ class utilsCog(commands.Cog):
 
 ## Allow use of cog class by main bot instance
 def setup(bot):
-    bot.add_cog(utilsCog(bot))
+    bot.add_cog(debugCog(bot))
---
+++ cogs/moderation.py
@@ -0,0 +1,33 @@
+## Initialization
+import discord
+from common import config, log
+from discord.ext import commands, tasks
+from discord.utils import get
+
+## Server moderation commands
+class moderationCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    ## Mutes target
+    @commands.command()
+    #@commands.has_permissions(mute_members=True)
+    async def mute(self, ctx, target: discord.Member):
+        muteOverwrite = discord.PermissionOverwrite()
+        muteOverwrite.add_reactions = False
+        muteOverwrite.send_messages = False
+        muteOverwrite.speak = False
+        muteOverwrite.stream = False
+        if target == ctx.author:
+            await ctx.send('You cannot mute yourself!')
+            return
+        for channel in ctx.guild.channels:
+            await channel.set_permissions(target, overwrite=muteOverwrite, reason=f'{target} was muted by {ctx.author}')
+        await ctx.send(f'{target} has been muted.')
+    ## Unmutes target
+    @commands.command()
+    async def unmute(self, ctx, target: discord.Member):
+        await ctx.send('nyi')
+## Allow use of cog class by main bot instance
+def setup(bot):
+    bot.add_cog(moderationCog(bot))
