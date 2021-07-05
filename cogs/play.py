fbc382af32b4e4b321281239ac0e62dafa5e2997
solunareclipse1 <26941050+solunareclipse1@users.noreply.github.com>
2021-07-05 14:06:07 -0500

    added very basic play command. todo: queue, playlist handling, auto search?

---
+++ cogs/play.py
@@ -0,0 +1,45 @@
+## Initialization
+import discord
+import youtube_dl
+from discord.ext import commands
+from common import config, log, embedMessage, ytdlSrc
+
+## Class setup
+class play(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        ## Help stuff
+        self.hidden = False
+        self.description = 'Plays the specified media in voice chat.'
+        self.usage = f"""
+        {config.cfg['options']['prefix']}play <link>
+        """
+
+    ## Command defining
+    @commands.command()
+    async def play(self, ctx, media):
+        mustJoin = False
+        if ctx.guild.me.voice == None:
+            mustJoin = True
+        elif ctx.author.voice.channel != ctx.guild.me.voice.channel:
+            embed = embedMessage.embed(
+                title = 'ERROR',
+                description = 'You must be connected to the same voice channel as the bot to play media.',
+                color = embedMessage.errorColor
+            )
+            await ctx.send(embed=embed)
+            return
+        if mustJoin == True:
+            self.connectedChannel = await ctx.author.voice.channel.connect()
+        ytdl_src = await ytdlSrc.ytdlSrc.from_url(media, loop=self.bot.loop, stream=True)
+        self.connectedChannel.play(ytdl_src, after=lambda e: print('Player error: %s' % e) if e else None)
+        embed = embedMessage.embed(
+            title = 'Now playing:',
+            description = ytdl_src.title
+        )
+        await ctx.send(embed=embed)
+
+## Allow use of cog class by main bot instance
+def setup(bot):
+    bot.add_cog(play(bot))
