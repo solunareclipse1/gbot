a22916bb6e7be394b21c520ecabbaed45a9b1f01
solunareclipse1 <26941050+solunareclipse1@users.noreply.github.com>
2021-07-09 11:03:44 -0500

    queue command finished (i think)

--- cogs/Music/queue.py
+++ cogs/Music/queue.py
@@ -1,7 +1,7 @@
 ## Initialization
 import discord
 from discord.ext import commands
-from common import config, log, embedMessage, category
+from common import config, log, embedMessage, category, misc
 
 ## Class setup
 class queue(commands.Cog):
@@ -18,7 +18,7 @@ class queue(commands.Cog):
 
     ## Command defining
     @commands.command()
-    async def queue(self, ctx):
+    async def queue(self, ctx, page=1):
         if self.bot.player.queue == []:
             embed = embedMessage.embed(
                 title = 'ERROR',
@@ -27,11 +27,31 @@ class queue(commands.Cog):
             )
             await ctx.send(embed=embed)
             return
-        q = "\n⁍ ".join(self.bot.player.queue[:10])
+        qLength = len(self.bot.player.queue)
+        qLower = ((page - 1) * 10) + 1
+        qUpper = page * 10
+        if page > 1 and qLower < qLength:
+            q = "\n⁍ ".join(self.bot.player.queue[qLower:qUpper])
+        elif page == 1:
+            q = "\n⁍ ".join(self.bot.player.queue[:qUpper])
+        elif qLower > qLength:
+            qUpper = int(misc.round_up(qLength, -1))
+            qLower = int(qUpper - 9)
+            page = int(qUpper / 10)
+            q = "\n⁍ ".join(self.bot.player.queue[qLower:qLength])
+        elif page < 1:
+            embed = embedMessage.embed(
+                title = 'ERROR',
+                description = 'Invalid page number!',
+                color = embedMessage.errorColor
+            )
+            await ctx.send(embed=embed)
+            return
         embed = embedMessage.embed(
             title = 'Queue',
             description = f'⁍ {q}',
-            color = embedMessage.defaultColor
+            color = embedMessage.defaultColor,
+            footer = f'Page {page} of {int((misc.round_up(qLength, -1)) / 10)}'
         )
         await ctx.send(embed=embed)
         return
