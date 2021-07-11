eaf8006959a4bb90d6ea9c1eade46dac598b9e96
quartzshard <matthew.burgess50@gmail.com>
2021-07-11 18:30:38 +0100

    Shut up selfDeaf

--- cogs/Music/selfDeaf.py
+++ cogs/Music/selfDeaf.py
@@ -14,8 +14,11 @@ class selfDeaf(commands.Cog):
     ## Self deafen when voice state changes.
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, pre, post):
-        if member == member.guild.me and member.voice.deaf == False:
-            await member.guild.me.edit(deafen=True)
+        try:
+            if member == member.guild.me and member.voice.deaf == False:
+                await member.guild.me.edit(deafen=True)
+        except AttributeError:
+            pass
 
 ## Allow use of cog class by main bot instance
 def setup(bot):
