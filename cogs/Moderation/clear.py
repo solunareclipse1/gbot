b44ea9a9272a0e20102c84d3849bd3221637c49f
solunareclipse1 <26941050+solunareclipse1@users.noreply.github.com>
2021-07-15 13:27:05 -0500

    major improvement to the clear command

--- cogs/Moderation/clear.py
+++ cogs/Moderation/clear.py
@@ -11,7 +11,7 @@ class clear(commands.Cog):
         ## Help stuff
         self.hidden = False
         self.category = category.getCategory(self.__module__)
-        self.description = 'Deletes the specified number of previous messages. Defaults to 1. Can be a little slow because of Discord\'s rate limiting.'
+        self.description = 'Deletes the specified number of previous messages. Defaults to 1.'
         self.usage = f"""
         {config.cfg['options']['prefix']}clear <number of messages>
         """
@@ -20,16 +20,24 @@ class clear(commands.Cog):
     @commands.command(aliases=['clean', 'delete', 'del', 'cls', 'remove', 'rem', 'd'])
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount=1):
-        delCount = -1
-        async for message in ctx.channel.history(limit=amount + 1):
-            await message.delete()
-            delCount = delCount + 1
-        embed = embedMessage.embed(
-            title = 'SUCCESS',
-            description = f'Deleted **{delCount}** messages.',
-            color = embedMessage.defaultColor
-        )
-        await ctx.send(embed=embed, delete_after=3.0)
+        if amount > 0:
+            await ctx.message.delete()
+            delCount = await ctx.channel.purge(limit=amount)
+            embed = embedMessage.embed(
+                title = 'SUCCESS',
+                description = f'Deleted **{len(delCount)}** message(s).',
+                color = embedMessage.defaultColor
+            )
+            await ctx.send(embed=embed, delete_after=3.0)
+        else:
+            embed = embedMessage.embed(
+                title = 'ERROR',
+                description = 'Delete amount must be more than zero!',
+                color = embedMessage.errorColor
+            )
+            await ctx.send(embed=embed)
+
+
         
 
 ## Allow use of cog class by main bot instance
