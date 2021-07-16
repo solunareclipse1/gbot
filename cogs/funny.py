bedbcfe3e326ab9dd5c43152a3a6df7d07048d08
solunareclipse1 <26941050+solunareclipse1@users.noreply.github.com>
2021-07-16 15:39:05 -0500

    more robotnik tweaks

--- cogs/funny.py
+++ cogs/funny.py
@@ -21,16 +21,19 @@ class funny(commands.Cog):
         likes = random.randint(0, 69420)
         rts = random.randint(0, 9001)
         if not target:
-            calloutPoster = '@Eggfucker1'
+            calloutPosterTag = '@Eggfucker1'
+            calloutPosterName = 'Dr. Ivo Robotnik'
             calledOut = '@xX_ShadoW_ThE_EdgehoG_Xx'
         elif target == ctx.me.mention:
-            calloutPoster = f'@{self.bot.user.display_name}'
+            calloutPosterTag = f'@{ctx.me.name}'
+            calloutPosterName = ctx.me.display_name
             calledOut = ctx.author.mention
         else:
-            calloutPoster = f'@{ctx.author.display_name}'
+            calloutPosterTag = f'@{ctx.author.name}'
+            calloutPosterName = ctx.author.display_name
             calledOut = target
         embed = embedMessage.embed(
-            title = f'Twitter: {calloutPoster}',
+            title = f'{calloutPosterName} ({calloutPosterTag})',
             description = f'I\'ve come to make an announcement: **{calledOut}\'s** a ***bitch-ass motherfucker***. He pissed on my **fucking** wife. That\'s right, he took his hedgehog fuckin\' quilly dick out and he ***pissed*** **on my** ***fucking*** **wife,** and he said his dick was ***\"THIS BIG\"***, and I said \"That\'s disgusting!\" So I\'m making a callout post on my Twitter dot com. ***{calledOut},*** **you got a** ***small*** **dick!** It\'s the size of this walnut except ***WAY smaller!*** And guess what? ***Here\'s what my dong looks like!*** That\'s right, baby! ***All*** points, ***no*** quills, ***no*** pillows, look at that, it looks like ***two balls*** **and a** ***bong!*** He fucked my wife, so *guess what?* **I\'m gonna fuck the Earth!** That\'s right, this is what you get, my ***SUPER LASER PISS!*** Except I\'m not gonna piss on the *Earth,* I\'m gonna go **higher.** I\'m pissing ***on the MOON!***\n\n***HOW DO YOU LIKE THAT, OBAMA?\nI PISSED ON THE MOON, YOU IDIOT!***\n\nYou have 23 hours before the piss *DRRRROPLLLETS* hit the ***fucking*** *Earth!* Now get out of my fucking sight, before I piss on you too!',
             color = discord.Color.from_rgb(42, 169, 224),
             thumbnail = 'https://cdn.discordapp.com/attachments/863527179479416852/865673772560416768/small-twitter-bird-icon_151340.png',
